acd58d504c9543529706fb3368d28b92c38dbeea
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-12-29 15:56:47 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { useGlobalContext } from './context'
+import Loading from './loading'
+import SetupForm from './setupForm'
+
 function App() {
-  return <h1> hello </h1>;
+  const { waiting, loading, index, questions } = useGlobalContext()
+  if (waiting) return <SetupForm />
+  if (loading) return <Loading />
+  return <main>quiz app</main>
 }
 
-export default App;
+export default App
--- src/context.js
+++ src/context.js
@@ -10,11 +10,51 @@ const table = {
 const url = ''
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(true)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => {})
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      }
+    } else {
+    }
+  }
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalContext = () => {
